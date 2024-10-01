db80c9e9062105f87fa3ef75ac8c87c53aa3d29d
solyaridz3 <solyaridz3@gmail.com>
2024-10-01 09:53:32 +0300

    ts routes

---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import authController from "../controllers/authController";
+import verifyToken from "../middleware/verifyToken";
+
+const router = Router();
+
+// Route to the home page
+router.get("/", (req, res) => {
+  res.render("home", { title: "Alias Game - Home" });
+});
+
+// Route to login page
+router.get("/login", authController.getLogin);
+
+// Route to register page
+router.get("/register", authController.getRegister);
+
+// Route to post login form
+router.post("/login", authController.postLogin);
+
+// Route to post register form
+router.post("/register", authController.postRegister);
+
+// Route to logout
+router.get("/logout", authController.getLogout);
+
+export default router;
---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,10 @@
+import{ Router } from "express";
+import userController from "../controllers/userController";
+
+const router = Router();
+
+router.get("/user/:username", userController.getUser);
+
+router.post("/user", userController.createUser);
+
+export default router;
