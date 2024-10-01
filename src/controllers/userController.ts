8b8ab0bfc98aa9456b7cbd2fb8dfabe5f972e0fd
solyaridz3 <solyaridz3@gmail.com>
2024-10-01 10:06:35 +0300

    ts controllers added

---
+++ src/controllers/userController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import User from "../models/userModel";
+
+export const createUser = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const newUser = await User.create(req.body);
+    console.log("User created:\n", newUser);
+  } catch (error: any) {
+    console.error(error.message);
+  }
+};
+
+export const getUser = async (
+  req: Request,
+  res: Response,
+): Promise<typeof User | null> => {
+  const { username } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      console.log(`No user with username: ${username}`);
+      return null;
+    }
+    console.log("Found user:\n", user);
+    return user;
+  } catch (error: any) {
+    console.error(error.message);
+    return null;
+  }
+};
