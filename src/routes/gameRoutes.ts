69507e2fad633e3da69dd3128218aeb75525b727
Przemyslaw Krepski <krepski80@gmail.com>
2024-10-03 17:45:25 +0200

    routes

--- src/routes/gameRoutes.ts
+++ src/routes/gameRoutes.ts
@@ -3,19 +3,28 @@ import gameController from "../controllers/gameController";
 
 const router = express.Router();
 
-// Tworzenie nowej gry
-// Dodaj trasę do renderowania formularza tworzenia gry
-router.get("/create", gameController.renderCreateGameForm);  // Wyświetlanie formularza
+// Route to render the form for creating a game
+router.get("/create", gameController.renderCreateGameForm);  
 
+// Route to create a new game
 router.post("/create", gameController.createGame);
 
-// Reszta tras (dodawanie użytkowników, aktualizacja punktów, czat)
+// Route to add a user to the game
 router.post("/addUser", gameController.addUser);
+
+// Route to update the score for a specific user in the game
 router.get("/:gameId/updateScore/:username/:points", gameController.updateScore);
+
+// Route to get the chat history for a specific game
 router.get("/:gameId/chat", gameController.getChatHistory);
+
+// Route to send a message in the game's chat
 router.post("/:gameId/chat/send", gameController.addMessageToChat);
+
+// Route to start a new turn in the game
 router.get("/:gameId/startTurn", gameController.startTurn);
 
 export default router;
+
 
 
